8d1f3deb04d1263b4044cd2c7469d8fdde706b94
resurgent888 <468264776@qq.com>
2019-01-18 15:05:15 +0800

    woshi

--- src/com/cslb/hlf/Test.java
+++ src/com/cslb/hlf/Test.java
@@ -1,7 +1,9 @@
-package com.clsb.bean;
+package com.cslb.hlf;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.clsb.bean.Person;
 
 public class Test {
 
@@ -19,9 +21,9 @@ public class Test {
 	      ApplicationContext context = new ClassPathXmlApplicationContext("abc.xml");
 		
 		 // 通过 bean 节点 调用 id 进行 注入 ， 返回 注入的对象
-		 Person perLCL = (Person) context.getBean("per");
+		 Person perhlf= (Person) context.getBean("per");
 		
-		 System.out.println(perLCL);
+		 System.out.println(perhlf);
 
 
 	}
